c16314ff7103461d91a920e93409ac3814955e55
Kacper Pucylo <pucylek@wp.pl>
2022-08-08 15:30:50 +0200

    trying stuff

--- mod01/part1/src/App.js
+++ mod01/part1/src/App.js
@@ -1,9 +1,17 @@
 
 const App = () => {
 	console.log("Frog")
+
+	const now = new Date()
+	const a = 10
+	const b = 5
+	
 	return (
 		<div>
-			<p>Hello world!</p>
+			<p>Hello world! It is {now.toString()}</p>
+			<p>
+				{a} + {b} is {a + b}
+			</p>
 		</div>
 	)
 }
