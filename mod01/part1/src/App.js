7f5657234f8b67024bc9fb710da61409b10ee5f2
Kacper Pucylo <pucylek@wp.pl>
2022-08-08 15:39:12 +0200

    trying stuff

--- mod01/part1/src/App.js
+++ mod01/part1/src/App.js
@@ -1,7 +1,7 @@
-const Hello = () => {
+const Hello = (props) => {
 	return (
 		<div>
-			<p>Hello world!</p>
+			<p>Hello {props.name}!</p>
 		</div>
 	)
 }
@@ -10,10 +10,10 @@ const App = () => {
 	return (
 		<div>
 			<h1>Greetings</h1>
-			<Hello />
-			<Hello />
-			<Hello />
-			<Hello />
+			<Hello name="frog"/>
+			<Hello name="toad"/>
+			<Hello name="turtle"/>
+			<Hello name="rat"/>
 		</div>
 	)
 }
