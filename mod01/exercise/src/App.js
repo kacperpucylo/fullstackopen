f48a8a4429becf665a232921fdfbdd2f3c7d2e96
Kacper Pucylo <pucylek@wp.pl>
2022-08-08 16:40:11 +0200

    trying stuff

--- mod01/exercise/src/App.js
+++ mod01/exercise/src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Header = (props) => {
+	return (
+		<h1>{props.course}</h1>
+	)
+}
+
+const Content = () => {
+	const part1 = "Fundamentals of React"
+	const exercises1 = 10
+	const part2 = "Using props to pass data"
+	const exercises2 = 7
+	const part3 = "State of a component"
+	const exercises3 = 14
+	
+	return (
+		<>
+			<p>
+				{part1} {exercises1}
+			</p>
+			<p>
+				{part2} {exercises2}
+			</p>
+			<p>
+				{part3} {exercises3}
+			</p>
+		</>
+	)
+}
+
+const Total = (props) => {
+	return (
+		<p>Number of exercises: {props.ex1 + props.ex2 + props.ex3}</p>
+	)
+}
+
+const App = () => {
+	const course = "Half Stack application development"
+	const part1 = "Fundamentals of React"
+	const exercises1 = 10
+	const part2 = "Using props to pass data"
+	const exercises2 = 7
+	const part3 = "State of a component"
+	const exercises3 = 14
+
+	return (
+		<div>
+			<Header course={course}/>
+			<Content />
+			<Total ex1={exercises1} ex2={exercises2} ex3={exercises3} />
+		</div>
+	)
 }
 
 export default App;
