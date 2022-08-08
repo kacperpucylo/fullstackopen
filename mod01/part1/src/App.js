8b044317f1c4e102fdfca5646a1d2e7671976697
Kacper Pucylo <pucylek@wp.pl>
2022-08-08 15:41:32 +0200

    trying stuff

--- mod01/part1/src/App.js
+++ mod01/part1/src/App.js
@@ -1,19 +1,19 @@
 const Hello = (props) => {
 	return (
 		<div>
-			<p>Hello {props.name}!</p>
+			<p>Hello {props.name}, you are {props.age} yo!</p>
 		</div>
 	)
 }
 
 const App = () => {
+	const name = "toad"
+	const age = 5
 	return (
 		<div>
 			<h1>Greetings</h1>
-			<Hello name="frog"/>
-			<Hello name="toad"/>
-			<Hello name="turtle"/>
-			<Hello name="rat"/>
+			<Hello name="frog" age={10}/>
+			<Hello name={name} age={age}/>
 		</div>
 	)
 }
