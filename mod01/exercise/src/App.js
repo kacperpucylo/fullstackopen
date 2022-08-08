4919416ab58340cebe878511f1733c31d2d48fef
Kacper Pucylo <pucylek@wp.pl>
2022-08-08 16:46:12 +0200

    trying stuff

--- mod01/exercise/src/App.js
+++ mod01/exercise/src/App.js
@@ -14,11 +14,11 @@ const Part = (props) => {
 const Content = (props) => {
 	
 	return (
-		<>
+		<div>
 			<Part part={props.part1} ex={props.ex1} />
 			<Part part={props.part2} ex={props.ex2} />
 			<Part part={props.part3} ex={props.ex3} />
-		</>
+		</div>
 	)
 }
 
