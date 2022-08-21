70cd1f9fbea1117efe4739a32844a0767563296a
Kacper Pucylo <pucylek@wp.pl>
2022-08-21 15:44:44 +0200

    mod02

--- mod02/ex2.6-2.10/src/App.js
+++ mod02/ex2.6-2.10/src/App.js
@@ -1,18 +1,16 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import List from './List'
 import Form from './Form'
 import Button from './Button'
+import personService from './services/persons'
+import Notification from './Notification'
 
 const App = () => {
-	const [persons, setPersons] = useState([
-		{ name: 'Arto Hellas', number: '040-123456', id: 1 },
-		{ name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-		{ name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-		{ name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-	  ])
+	const [persons, setPersons] = useState([])
 
   const [newName, setNewName] = useState('')
   const [newNumber, setNumber] = useState('')
+  const [message, setMessage] = useState(null)
 
   const handleNameChange = (event) => {
 	setNewName(event.target.value)
@@ -29,20 +27,35 @@ const App = () => {
 		if (person.name === newName) {return true}
 		return false;
 	})) {
-		setPersons(persons.concat(arr))
+		personService.create(arr).then(response => {
+			setMessage(`${newName} has been added`)
+			setTimeout(() => {
+				setMessage(null)
+			}, 5000)
+			setPersons(persons.concat(response.data))
+			setNewName('')
+			setNumber('')
+		})
 	}
 	else {
 		window.alert(`${newName} is already added to the phonebook`)
 	}
   }
 
+  useEffect(() => {
+	personService.getAll().then(response => {
+		setPersons(response.data)
+	})
+  }, [])
+
   return (
     <div>
 		<h2>Phonebook</h2>
+		<Notification message={message} />
     	<Form name={newName} handleName={handleNameChange} number={newNumber} handleNumber={handleNumberChange}/>
 		<Button handleSubmit={handleSubmit}/>
     	<h2>Numbers</h2>
-		<List persons={persons} />
+		<List persons={persons} set={setPersons} />
     </div>
   )
 }
--- mod02/ex2.6-2.10/src/List.js
+++ mod02/ex2.6-2.10/src/List.js
@@ -1,8 +1,25 @@
-const List = ({persons}) => {
+import personService from './services/persons'
+
+const List = ({persons, set}) => {
+
+	const delHandler = (id) => {
+		if (window.confirm("Delete contact?"))
+		{
+			personService.del(id).then(() => personService.getAll().then(response => {
+				set(response.data)
+			}))
+		}
+	}
+
 	return (
 		<div>
 			{persons.map((person) => (
-				<p key={person.name}>{person.name}&nbsp;{person.number}</p>
+				<div>
+					<p key={person.id}>
+						{person.name}&nbsp;{person.number}&nbsp;&nbsp;
+						<button type="submit" onClick={() => {delHandler(person.id)}}>delete</button>
+					</p>
+				</div>
 			))}
 		</div>
 	)
